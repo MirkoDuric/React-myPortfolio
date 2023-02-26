fef52f62591d98ae34590fd7f3c2ec0d2a2ca7c0
MirkoDuric <mirkodjuricczv@gmail.com>
2023-02-26 15:59:56 +0100

    Portfolio needs some finishing touch and ajustment on responsiv design. Projects to be uploaded

--- src/Components/contact-form/ContactForm.js
+++ src/Components/contact-form/ContactForm.js
@@ -2,36 +2,66 @@ import MenuButton from "../MenuButton";
 import MenuSidebar from "../MenuSidebar";
 import { useState } from "react";
 import "./Contact.css";
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
+
 export default function ContactForm() {
   const [active, setActive] = useState(false);
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_vtkajy2",
+        "template_6tuqz25",
+        form.current,
+        "tpLiBVvKMei9R332a"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          e.target.reset();
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
 
   return (
     <section className="contact">
       <div className="contact-title">
         <h2>Contact Me</h2>
       </div>
-      <div className="contactForm">
+      <form ref={form} onSubmit={sendEmail} className="contactForm">
         <div className="row">
           <div className="col50">
-            <input type="text" name="" placeholder="First Name"></input>
+            <input
+              type="text"
+              name="First_name"
+              placeholder="First Name"
+            ></input>
           </div>
           <div className="col50">
-            <input type="text" name="" placeholder="Last Name"></input>
+            <input type="text" name="Last_name" placeholder="Last Name"></input>
           </div>
         </div>
         <div className="row">
           <div className="col50">
-            <input type="text" name="" placeholder="Email"></input>
+            <input type="text" name="email" placeholder="Email"></input>
           </div>
           <div className="col50">
-            <input type="text" name="" placeholder="Phone"></input>
+            <input type="text" name="phone" placeholder="Phone"></input>
           </div>
         </div>
         <div className="row">
           <div className="col100">
             <textarea
               type="text"
-              name=""
+              name="message"
               id=""
               cols="30"
               rows="10"
@@ -44,7 +74,7 @@ export default function ContactForm() {
             <input type="submit" value="Send" />
           </div>
         </div>
-      </div>
+      </form>
       <MenuButton
         activeTggle={(e) => {
           return setActive(!active), e.target.classList.toggle("active");
--- src/Components/my-projects/MyProjects.js
+++ src/Components/my-projects/MyProjects.js
@@ -3,45 +3,51 @@ import { useState } from "react";
 import MenuButton from "../MenuButton";
 import MenuSidebar from "../MenuSidebar";
 import ProjectCard from "./Project-Card";
-import myImg from "../img/meandthedog.jpg";
+import myImg from "../img/myportrait.jpg";
 export default function MyProjects() {
   const [active, setActive] = useState(false);
   const projectData = [
     {
+      id: 1,
       projectName: "Pomodoro-app",
       projectDescription:
         "The Pomodoro Technique is a time management method developed by Francesco Cirillo in the late 1980s.[1] It uses a kitchen timer to break work into intervals, typically 25 minutes in length, separated by short breaks. Each interval is known as a pomodoro, from the Italian word for tomato, after the tomato-shaped kitchen timer Cirillo used as a university student.",
-      projectImg: "../img/meandthedog.jpg",
+      projectImg: require("../img/myportrait.jpg"),
     },
     {
+      id: 2,
       projectName: "Pomodoro-app",
       projectDescription:
         "The Pomodoro Technique is a time management method developed by Francesco Cirillo in the late 1980s.[1] It uses a kitchen timer to break work into intervals, typically 25 minutes in length, separated by short breaks. Each interval is known as a pomodoro, from the Italian word for tomato, after the tomato-shaped kitchen timer Cirillo used as a university student.",
-      projectImg: "../img/meandthedog.jpg",
+      projectImg: require("../img/myportrait.jpg"),
     },
     {
+      id: 3,
       projectName: "Pomodoro-app",
       projectDescription:
         "The Pomodoro Technique is a time management method developed by Francesco Cirillo in the late 1980s.[1] It uses a kitchen timer to break work into intervals, typically 25 minutes in length, separated by short breaks. Each interval is known as a pomodoro, from the Italian word for tomato, after the tomato-shaped kitchen timer Cirillo used as a university student.",
-      projectImg: "../img/meandthedog.jpg",
+      projectImg: require("../img/myportrait.jpg"),
     },
     {
+      id: 4,
       projectName: "Pomodoro-app",
       projectDescription:
         "The Pomodoro Technique is a time management method developed by Francesco Cirillo in the late 1980s.[1] It uses a kitchen timer to break work into intervals, typically 25 minutes in length, separated by short breaks. Each interval is known as a pomodoro, from the Italian word for tomato, after the tomato-shaped kitchen timer Cirillo used as a university student.",
-      projectImg: "../img/meandthedog.jpg",
+      projectImg: require("../img/myportrait.jpg"),
     },
     {
+      id: 5,
       projectName: "Pomodoro-app",
       projectDescription:
         "The Pomodoro Technique is a time management method developed by Francesco Cirillo in the late 1980s.[1] It uses a kitchen timer to break work into intervals, typically 25 minutes in length, separated by short breaks. Each interval is known as a pomodoro, from the Italian word for tomato, after the tomato-shaped kitchen timer Cirillo used as a university student.",
-      projectImg: "../img/meandthedog.jpg",
+      projectImg: require("../img/myportrait.jpg"),
     },
     {
+      id: 6,
       projectName: "Pomodoro-app",
       projectDescription:
         "The Pomodoro Technique is a time management method developed by Francesco Cirillo in the late 1980s.[1] It uses a kitchen timer to break work into intervals, typically 25 minutes in length, separated by short breaks. Each interval is known as a pomodoro, from the Italian word for tomato, after the tomato-shaped kitchen timer Cirillo used as a university student.",
-      projectImg: "../img/meandthedog.jpg",
+      projectImg: require("../img/myportrait.jpg"),
     },
   ];
 
@@ -59,9 +65,10 @@ export default function MyProjects() {
         {projectData.map((project) => {
           return (
             <ProjectCard
+              key={project.id}
               projectName={project.projectName}
               projectDescription={project.projectDescription}
-              projectImg={project.projectDescription}
+              projectImg={project.projectImg}
             />
           );
         })}
