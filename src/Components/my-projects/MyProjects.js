06c1597493f7e5c61f662497e388bf01511fb667
MirkoDuric <mirkodjuricczv@gmail.com>
2023-05-30 00:01:10 +0200

    New project titles

--- src/Components/my-projects/MyProjects.js
+++ src/Components/my-projects/MyProjects.js
@@ -23,28 +23,28 @@ export default function MyProjects() {
     },
     {
       id: 3,
-      projectName: "Pomodoro-app",
+      projectName: "Restourants in Köln",
       projectDescription:
         "The Pomodoro Technique is a time management method developed by Francesco Cirillo in the late 1980s.[1] It uses a kitchen timer to break work into intervals, typically 25 minutes in length, separated by short breaks. Each interval is known as a pomodoro, from the Italian word for tomato, after the tomato-shaped kitchen timer Cirillo used as a university student.",
       projectImg: require("../img/myportrait.jpg"),
     },
     {
       id: 4,
-      projectName: "Pomodoro-app",
+      projectName: "Movies app",
       projectDescription:
         "The Pomodoro Technique is a time management method developed by Francesco Cirillo in the late 1980s.[1] It uses a kitchen timer to break work into intervals, typically 25 minutes in length, separated by short breaks. Each interval is known as a pomodoro, from the Italian word for tomato, after the tomato-shaped kitchen timer Cirillo used as a university student.",
       projectImg: require("../img/myportrait.jpg"),
     },
     {
       id: 5,
-      projectName: "Pomodoro-app",
+      projectName: "What's my IP adress",
       projectDescription:
         "The Pomodoro Technique is a time management method developed by Francesco Cirillo in the late 1980s.[1] It uses a kitchen timer to break work into intervals, typically 25 minutes in length, separated by short breaks. Each interval is known as a pomodoro, from the Italian word for tomato, after the tomato-shaped kitchen timer Cirillo used as a university student.",
       projectImg: require("../img/myportrait.jpg"),
     },
     {
       id: 6,
-      projectName: "Pomodoro-app",
+      projectName: "Gig guide",
       projectDescription:
         "The Pomodoro Technique is a time management method developed by Francesco Cirillo in the late 1980s.[1] It uses a kitchen timer to break work into intervals, typically 25 minutes in length, separated by short breaks. Each interval is known as a pomodoro, from the Italian word for tomato, after the tomato-shaped kitchen timer Cirillo used as a university student.",
       projectImg: require("../img/myportrait.jpg"),
