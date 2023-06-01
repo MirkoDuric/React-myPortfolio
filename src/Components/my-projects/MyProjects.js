e7d6d9f60bcdc617d155a03a39d0d5a47acba205
MirkoDuric <mirkodjuricczv@gmail.com>
2023-06-01 19:25:43 +0200

    Adding some description to the projects and connecting the link to my github.

--- src/Components/my-projects/MyProjects.js
+++ src/Components/my-projects/MyProjects.js
@@ -11,42 +11,42 @@ export default function MyProjects() {
       id: 1,
       projectName: "ToDo List",
       projectDescription:
-        "This was my first biger project. I must say that this was one of the most interesting projects to wokr on. It's plain JS, with no DB. I learned a lot during this project and really enjoyed to work in this team!",
+        "This was my first biger project. I must say that this was one of the most interesting projects to work on. It's plain JS, with no DB. I learned a lot during this project and really enjoyed to work in this team! Our goal was to create ToDo List, we also created some login form, but the user would only be saved in a objects array and would disapier once the page is reloaded.",
       projectImg: require("../img/myportrait.jpg"),
     },
     {
       id: 2,
       projectName: "RecipesInc",
       projectDescription:
-        "This project was my first DB project. I used React, PostgreQL as DB and plain CSS. Project shows 3 dishes from three diferent cousines. Preparation and ingredients. Take a look, you might as well get the idea on what to cook this week!!",
+        "This project was my first DB project. I used React, PostgreQL as DB and plain CSS. Project shows 3 dishes from three diferent cousines. Preparation and ingredients. Main goal of this project was creating my own server and connecting my DB with my front end. Also learning how to work with realtional DB. Take a look, you might as well get the idea on what to cook this week!! ",
       projectImg: require("../img/myportrait.jpg"),
     },
     {
       id: 3,
       projectName: "Restourants in Köln",
       projectDescription:
-        "The Pomodoro Technique is a time management method developed by Francesco Cirillo in the late 1980s.[1] It uses a kitchen timer to break work into intervals, typically 25 minutes in length, separated by short breaks. Each interval is known as a pomodoro, from the Italian word for tomato, after the tomato-shaped kitchen timer Cirillo used as a university student.",
+        "This project was a group project, and our goal was to use server created by one of us and to practice API cals in our front end. We decided to place couple of restaurants from Köln in our DB, and to get info about them on our front end. The project was done in the phase of our Bootcamp where we were learning API cals. erver and DB were done by our more experienced cloegue. ",
       projectImg: require("../img/myportrait.jpg"),
     },
     {
       id: 4,
       projectName: "Movies app",
       projectDescription:
-        "The Pomodoro Technique is a time management method developed by Francesco Cirillo in the late 1980s.[1] It uses a kitchen timer to break work into intervals, typically 25 minutes in length, separated by short breaks. Each interval is known as a pomodoro, from the Italian word for tomato, after the tomato-shaped kitchen timer Cirillo used as a university student.",
+        "In this project I created my own little Movies DB. Every time I would like to watch something I can inform myself about the movie on my own site. Goal of this project was to practice my skills, to refresh my memory on some topics and to improve my coding. I used open API, plain CSS, React(JSX).",
       projectImg: require("../img/myportrait.jpg"),
     },
     {
       id: 5,
       projectName: "What's my IP adress",
       projectDescription:
-        "The Pomodoro Technique is a time management method developed by Francesco Cirillo in the late 1980s.[1] It uses a kitchen timer to break work into intervals, typically 25 minutes in length, separated by short breaks. Each interval is known as a pomodoro, from the Italian word for tomato, after the tomato-shaped kitchen timer Cirillo used as a university student.",
+        "Here I am using two diferent open API's. This way I'm taking the IP adress of the person who is using the link, and based on that IP address locating them and presenting some basic data. Location, time, coutnry flag, and their provider. It's also one of the projects where we learned how to use API and work with data that we get.",
       projectImg: require("../img/myportrait.jpg"),
     },
     {
       id: 6,
       projectName: "Gig guide",
       projectDescription:
-        "The Pomodoro Technique is a time management method developed by Francesco Cirillo in the late 1980s.[1] It uses a kitchen timer to break work into intervals, typically 25 minutes in length, separated by short breaks. Each interval is known as a pomodoro, from the Italian word for tomato, after the tomato-shaped kitchen timer Cirillo used as a university student.",
+        "Gig Guide was a final project of my Bootcamp. Idea behind this project is tracking music events in your hometown. We used open API from Ticketmaster present more popular artists and their events. But also we created the option to sign up and create your own events if your an artist. This way local bands would be able to present their gigs, and we would have an easy way to get info about cool bar gigs to visit! ",
       projectImg: require("../img/myportrait.jpg"),
     },
   ];
@@ -71,9 +71,13 @@ export default function MyProjects() {
             These projects are not at it's finest. Some of them I left like that
             in order to show mylearning progres. If you click on the "Take a
             look" button it will take you to the site itself. But if you would
-            like to see my github profile, just click here. I have set irelevant
-            repositories to private so you don't lose time while serching for
-            projects where you can actualy see something."
+            like to see my github profile, just click{" "}
+            <a href="https://github.com/MirkoDuric" target="_blank">
+              here
+            </a>
+            . I have set irelevant repositories to private so you don't lose
+            time while serching for projects where you can actualy see
+            something."
             <span className="element-p-span">{"</p>"}</span>
           </p>
           <p className="third-intro-paragraph">
